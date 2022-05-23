57217e6db02a1e60e60702b7b28aebe3ca4c0041
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-23 18:02:16 +0300

    start

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+gallery.addEventListener('click', lightbox);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`;
+    })
+    .join('');
+}
