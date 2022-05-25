f8c485c7fd9526555b71242ccb5ce367dfa17067
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-25 21:23:54 +0300

    fixes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,13 @@
-// import Vimeo from 'Vimeo.Player';
+import Player from '@vimeo/player';
+
 import throttle from 'lodash.throttle';
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
 const currentTimeStorage = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
 
 console.log(currentTimeStorage);
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+const player = new Player('vimeo-player');
 // iframe.addEventListener('play', evt => {
 //   console.log(evt);
 // });
