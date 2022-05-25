28269c44dfc79e303f7e7e64960c5e0455a84fd9
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-25 15:45:02 +0300

    t3 fin

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const dataStorage = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener(
+  'input',
+  throttle(evt => {
+    dataStorage[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
+  }, 500),
+);
+refs.form.addEventListener('submit', onFormSubmit);
+
+savedTextArea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(dataStorage);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function savedTextArea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  //   console.log(savedMessage);
+  const parsedMessage = JSON.parse(savedMessage);
+  if (parsedMessage) {
+    refs.email.value = parsedMessage.email;
+    refs.textarea.value = parsedMessage.message;
+    // console.log(parsedMessage);
+  }
+}
